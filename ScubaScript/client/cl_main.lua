ca25d2aeb4b9a0c043991db9b22f1e4e3096f139
Thorvald <thorvaldvanbeek193@gmail.com>
2024-05-15 13:35:32 +0200

    Added updates via chat, rewrote function

--- ScubaScript/client/cl_main.lua
+++ ScubaScript/client/cl_main.lua
@@ -6,7 +6,7 @@ DiveMinutes = 20
 
 divetimeS = DiveMinutes*60
 scubaTog = false
-
+local DivingTime = divetimeS
 
 
 RegisterCommand('dive', function()
@@ -16,15 +16,37 @@ end, false)
 
 function dive()
     if not scubaTog then
-        scubaTog = true
-            Citizen.Wait(5)
-    
-            SetPedDiesInWater(PlayerPedId(), false)
-            Citizen.Wait(divetimeS*1000)
+        scubaTog = true        
+        Citizen.Wait(5)
 
+        SetPedDiesInWater(PlayerPedId(), false)
+        TriggerEvent('chat:addMessage', {
+            color = { 224, 96, 4 },
+            multiline = true,
+            args = {"ScubaScript", "OxygenTank Activated!"}
+        })
+        while (DivingTime ~= 0) do
+            Citizen.Wait(1000)
+            if (DivingTime == 120) then
+                TriggerEvent('chat:addMessage', {
+                    color = { 224, 96, 4 },
+                    multiline = true,
+                    args = {"ScubaScript", "OxygenTank Running On Empty! Surface Now!"}
+                })
+                DivingTime = (DivingTime - 1)
+            elseif (DivingTime == 1) then
+            DivingTime = (DivingTime - 1)
             scubaTog = false
-
+        
             SetPedDiesInWater(PlayerPedId(), true)
-
+            TriggerEvent('chat:addMessage', {
+                color = { 224, 96, 4 },
+                multiline = true,
+                args = {"ScubaScript", "OxygenTank Empty! Change Tank!"}
+            })
+            else
+                DivingTime = (DivingTime - 1)
+            end
         end
+    end
 end
--- ScubaScript/fxmanifest.lua
+++ ScubaScript/fxmanifest.lua
@@ -5,8 +5,8 @@ lua54 'yes'
 
 author 'Eric Snail'
 description 'Resource that lets you swim underwater for X amount of minutes without dying bc of lack of air.'
-version '1.0.0'
+version '1.1.0'
 
 client_scripts {
-    '/client/*.lua',
+    '/client/cl_*.lua',
 }
